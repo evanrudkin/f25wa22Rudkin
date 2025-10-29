00ba0ab3125c25e92a9dd5807a41ffc4fef61578
evanrudkin <evanvinzant1@gmail.com>
2025-10-29 13:27:32 -0500

    Changed Users Endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+// Create two variables for this endpoint
+let factor = 2;
+let current = 25;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  current = current * factor; // multiply each time you refresh
+  res.send(`Current is: ${current}`);
 });
 
 module.exports = router;
